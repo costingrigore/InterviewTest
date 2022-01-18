f12942789b21a7b80cb02ce27c533bee9c9b0b4d
Costin Grigore <costingrigore6@gmail.com>
2022-01-18 22:21:39 +0000

    Added API queries to backend

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
+const numbers = [1, 2, 3, 5, 5];
+
+const updatedNums = numbers.map((number) => {
+    return <li>{number}</li>;
+});
+
+const sqlite3 = require('sqlite3').verbose();
+let db = new sqlite3.Database('./SqliteDB.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+});
+db.close();
+
 export default class App extends Component {
 
   render () {
     return (
-      <div>Complete your app here</div>
+        <div>Complete your app here</div>,
+        <ul>
+            {updatedNums}
+        </ul >
     );
   }
 }
